a3e0cffcaa9a7ac035c5a35dba92a371e2833660
Ashley Coleman <ashley@yieldify.com>
2015-03-01 15:08:13 +0000

    initial commit

---
+++ simple-todos.js
@@ -0,0 +1,17 @@
+Tasks = new new Mongo.Collection("tasks");
+if (Meteor.isClient) {
+ 
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    
+  });
+}
